e99fd50c4cc2c4d932cf0168c95074491bcd183d
02daniel <dan-bom@hotmail.com>
2014-02-13 19:33:33 -0600

    Update JamesBond.java

--- JamesBond.java
+++ JamesBond.java
@@ -3,6 +3,7 @@
  * Programar agente con almenos un comportamiento
  * autores:
  * 	Alberto Cortes Blanco		1478824
+ *  Daniel Ivan de Anda Becerra. 1612155
  */
 import jade.core.Agent;
 import jade.core.behaviours.*;
@@ -12,6 +13,7 @@ public class JamesBond extends Agent{
     protected void setup(){
     //llamada al comportamiento
         addBehaviour(new Comportamiento1());
+        addBehaviour(new Comportamiento2());
     }
     
     
@@ -27,4 +29,43 @@ public class JamesBond extends Agent{
             return true;
         }
     }
+    
+    	 //Finalizacion del agente
+	protected void takeDown(){
+		System.out.println("La escala ha terminado");
+	}
+	
+	//Definicion de un comportamiento
+	private class Comportamiento2 extends Behaviour{
+		private int estado = 0;
+		
+		public void action(){
+			switch(estado){
+			case 0: System.out.println("Estaba");
+			break;
+			case 1: System.out.println("una");
+			break;
+			case 2: System.out.println("rana");
+			break;
+			case 3: System.out.println("sentada");
+			break;
+			case 4: System.out.println("debajo");
+			break;
+			case 5: System.out.println("del agua");
+			break;
+			case 6:{
+				System.out.println("Y cuando");
+				myAgent.doDelete();
+				break;
+			}
+			
+			}
+			estado++;
+		}
+			public boolean done(){
+				return (estado > 7);
+			
+		}
+	}
+
 }
